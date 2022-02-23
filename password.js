83d5cf9d997fbe0150408df0a8127f8074b2ddc7
Michael Nowlin <michaelnowlin5@gmail.com>
2022-02-23 01:37:49 -0700

    checking if my password validates what is right and wrong about the password.

--- password.js
+++ password.js
@@ -1,8 +1,9 @@
 console.log("hello User, we welcome you to our Password Validator Tool");
 console.log("A valid password must: ");
 console.log("be at least 10 spaces long.");
-console.log("have at least 1 capatial letter and lower case letter.");
+console.log("have at least 1 capatial letter.");
 console.log("have at least 1 number or symbol.");
+//this was the way to have the user give input.
 /**  
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -19,6 +20,10 @@ const readline = require('readline').createInterface({
     readline.close();
   });
  */
+// this has the user input hidden so others cant see what they are typing.
+  var capatial = false;
+  var numOrSymbol = false;
+  var character = '';
   var Writable = require('stream').Writable;
   
   var mutableStdout = new Writable({
@@ -38,6 +43,35 @@ const readline = require('readline').createInterface({
   });
   
   readline.question('Password: ', function(password) {
+     
+    for (let i = 0; i < password.length; i++){
+        character = password.charAt(i);
+              
+        if (character === character.toUpperCase()){
+            capatial = true;
+        }
+        if (character === !NaN){
+            numOrSymbol = true;
+        }
+
+    }
+    if (password.length >= 10 && capatial === true && numOrSymbol === true ){
+        console.log("password is valid");
+      } else if (password.length < 10 && capatial === true && numOrSymbol === true ) {
+        console.log("password is invalid. you were under 10 spaces.");
+      } else if (password.length >= 10 && capatial === false && numOrSymbol === true ){
+          console.log("password is invalid. ther was no capatial.");
+      } else if (password.length >= 10 && capatial === true && numOrSymbol === false ){
+          console.log("password is invalid. you did not put in a number or symbol");
+      } else if (password.length < 10 && capatial === false && numOrSymbol === true ){
+          console.log("password was invalid. you did not have 10 spaces and no capatial");
+      } else if (password.length < 10 && capatial === true && numOrSymbol === false ){
+          console.log("password invalid. you did not do 10 spaces and no number or symbol.")
+      } else if (password.length >= 10 && capatial === false && numOrSymbol === false ){
+          console.log("password is invalid. you did not have a capatial and not number or symbol.")
+      } else {
+          console.log("password invalid.")
+      }
     console.log('\nPassword is ' + password);
     readline.close();
   });
