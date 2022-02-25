ab554d4006fb63d1ae57f51ce61456d27ae958da
Michael Nowlin <michaelnowlin5@gmail.com>
2022-02-24 19:12:30 -0700

    finished defining the sldc in my own words.

--- sldc.js
+++ sldc.js
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "this is the starting step, where u taalk with your stakeholder and such about what the problem that need to be solved is.";
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const design = "set up a design of the project to see if this is what we want to make to solve the problem and talk with the client and see what changes need to be done before starting on creating the solution.";
+const implementation = "start to create the solution from the design that was made. this is when you start coding and make the solution.";
+const testingAndIntegration = "we now check the code and see if any errors appear and checking for ways the code or the solution could break and change and fix the issues that rise from this."
+const maintain = "Now is when the developers keep the program running/ repeating the cyle again for the solution and keeping the code up to date.";
